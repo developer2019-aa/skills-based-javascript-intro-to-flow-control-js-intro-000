49ea388f9783175073293e3f009b62927af2a97d
Allen <developer2019@icloud.com>
2020-05-07 04:09:22 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,42 @@
 function basicTeenager(age) {
-
+  if (age < 18 === true && age > 12 === true) {
+    return 'You are a teenager!'
+  }
 }
 
 function teenager(age) {
-
+  if (age < 18 === true && age > 12 === true) {
+    return 'You are a teenager!'
+  } else {
+     return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
+  if (age < 18 === true && age > 12 === true) {
+    return 'You are a teenager!'
+  } else if (age < 13) {
+     return'You are a kid'
+  } else {
+    return'You are a grownup'
+  }
 
 }
 
 function ternaryTeenager(age) {
+  if (age < 18 === true && age > 12 === true) {
+    return 'You are a teenager'
+  } else {
+    return 'You are not a teenager'
+  }
 
 }
 
 function switchAge(age) {
-
+  switch (age) {
+    case (13, 14, 15, 16, 17, 18, 19):
+      return "You are a teenager"
+    default:
+      return "You have an age"
+  }
 }
